6099d6d1ec613aaa1891b93ea6d015f9ac5e8968
RobertZapata <101352118+RobertZapata@users.noreply.github.com>
2022-04-11 13:49:51 -0500

    First Commit

---
+++ src/app/pages/users/users-routing.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { UserEditComponent } from './user-edit/user-edit.component';
+import { UserCreateComponent } from './user-create/user-create.component';
+import { UserListComponent } from './user-list/user-list.component';
+
+const routes: Routes = [
+  {path: 'useredit', component: UserEditComponent},
+  {path: 'usercreate', component: UserCreateComponent},
+  {path: '', component: UserListComponent}
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
+})
+export class UsersRoutingModule { }
